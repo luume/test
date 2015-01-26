ebcf0229afa84fc03c4dc839652421ec6323e8ad
luume <darkones@naver.com>
2015-01-26 14:10:17 +0900

    gdg
    gd

---
+++ chat1/app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app =express();
+
+var server= require('http').createServer(app);
+
+io = require('socket.io').listen(server);
+
+server.listen(80);
+
+app.get('/', function(req, res){
+    res.sendfile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function(socket){
+    socket.on('send message', function(data){
+        console.log(data);
+        io.sockets.emit('new message', data);
+    });
+});
